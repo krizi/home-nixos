0ff0b60f0a71c3c39de891408286a590b45af6c0
krizi <krazy2krizi@gmail.com>
2025-12-25 17:56:27 +0100

    fix: allow kubernetes user to read k3s-config

--- modules/k3s-server.nix
+++ modules/k3s-server.nix
@@ -1,13 +1,15 @@
 { config, pkgs, ... }:
 
 {
-  environment.variables.K3S_KUBECONFIG_MODE = "644";
-
   systemd.services.k3s-server = {
     description = "k3s server";
     wantedBy = [ "multi-user.target" ];
     after = [ "network-online.target" ];
     wants = [ "network-online.target" ];
+
+    environment = {
+      K3S_KUBECONFIG_MODE = "644";
+    };
 
     serviceConfig = {
       Type = "notify";
@@ -17,10 +19,7 @@
           --disable traefik \
           --node-name ${config.networking.hostName}
       '';
-    ExecStartPost = ''
-      chmod 644 /etc/rancher/k3s/k3s.yaml
-      chown root:kubernetes /etc/rancher/k3s/k3s.yaml
-    '';
+
       Restart = "always";
       RestartSec = 5;
 
