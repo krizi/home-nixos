6d9a40172dabee5d635dee02ae6af77fd61376cc
krizi <krazy2krizi@gmail.com>
2025-12-25 03:11:25 +0100

    fix: adjust filesystem

--- modules/user-kubernetes.nix
+++ modules/user-kubernetes.nix
@@ -6,7 +6,8 @@
         extraGroups = [ "wheel" ];
     };
 
-
+boot.initrd.supportedFilesystems = [ "ext4" ];
+boot.supportedFilesystems = [ "ext4" ];
   fileSystems."/" = {
     device = "/dev/vda2";
     fsType = "ext4";
