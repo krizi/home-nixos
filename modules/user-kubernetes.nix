d0ef64e96acd97c6d38839e398f39d268d44a3c7
krizi <krazy2krizi@gmail.com>
2025-12-25 02:51:05 +0100

    chore: try simpler

--- modules/user-kubernetes.nix
+++ modules/user-kubernetes.nix
@@ -1,44 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  users.users.kubernetes = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ];
-    shell = pkgs.zsh;
-
-    openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDFky8y1fjgWSXlAm78e4sAyeS/kX1BJ2ykS5qX4arbAGM5ICvID564HvGVtB9hb7DXnKBNgbjYfkOYJkOWf5cx4Rrhhd9MoBzOdN+4pG2wGg8Ghu5cY1qIS8NCjA7U6tYU21hB250J7kD/OUhm2ZbIopXw7+MFhxpX5CtXBvfQHwVZhoD9/+AtA5f01wxekQyIgB2kSUBtp3RgQ1qJppMePd93wRIpnmpMTRz4LYHcR278N/MoJ1TRsQPFTfJAk46+xA3wOVd464ai4PHqNrwHNET5DwxtL7b0G8OfkuA9YoYlZds435Oq3Ob9icdbCkROzL/AtQVoA0hAskRNnDGAd8DH0jWJqHbLbVK2m+b88mNT6fSEh4hoBikB5lPtwbUysYpnPTDK5VWoPbgcK85G1szAFOYVBBoPQbRUYLADG54n27HhuY/T4/bmRTx21KggLr/2l/ehqezd6O5XRf7R554bPC2AbLuN/x1WTz7SP/M/VI9Vlq3x2pymO241C2hUrFahWupfsd2eGZiwf8x4Eb4En7RDO2JTcT6iXQ9Ob4ZjLSUYbpRibUcWMHqK+5wc3ND4GUlSQlgR8IdbbGvyKkgU9uRwDOtpwrXAeoqcu93kU5ML+PjeHEf9MD5B5dVVyqHAj4S1ZgjuWKzSjHRpmwBdCvdcVL5tVkbdnOPidw== kluster auth"
-    ];
+  home = {
+    username = "kubernetes";
+    homeDirectory = "/home/kubernetes";
   };
 
-  ########################
-  # Home-Manager für "kubernetes"
-  ########################
-
-  home-manager.users.kubernetes = { pkgs, ... }: {
-    home.username = "kubernetes";
-    home.homeDirectory = "/home/kubernetes";
-
-    programs = {
-      zsh = {
-        enable = true;
-#        ohMyZsh = {
-#          enable = true;
-#          theme = "robbyrussell";
-#          plugins = [
-#            "sudo"
-#            "terraform"
-#            "systemadmin"
-#            "vi-mode"
-#          ];
-#        };
-      };
-      git = {
-        enable = true;
-      };
-    };
-
-
-    home.stateVersion = "24.05";
-  };
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
 }
