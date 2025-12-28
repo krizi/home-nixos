8db26faadf94719f812b976286bab350050081ec
krizi <krazy2krizi@gmail.com>
2025-12-28 17:25:32 +0100

    fix: disable emulated platforms on master

--- hosts/k8s-master-01.nix
+++ hosts/k8s-master-01.nix
@@ -17,6 +17,6 @@
   };
 
   # used to build rpi image
-  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
-  nix.settings.extra-platforms = [ "aarch64-linux" ];
+  #  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  #  nix.settings.extra-platforms = [ "aarch64-linux" ];
 }
