353480c2d2c7839b5e8c9bcb10fe3f32278245f2
krizi <krazy2krizi@gmail.com>
2025-12-29 19:47:02 +0100

    feat: switch to k0s using existing module

--- hosts/k8s-master-01.nix
+++ hosts/k8s-master-01.nix
@@ -9,6 +9,23 @@
 
   networking.hostName = "k8s-master-01";
 
+  services.k0s = {
+    enable = true;
+    isLeader = true;
+    role = "controller";
+    clusterName = "hellga";
+    # typischer Datenordner
+    dataDir = "/var/lib/k0s";
+
+    spec = {
+      network = {
+        kubeProxy = {
+          disabled = true;
+        };
+      };
+    };
+  };
+
   # used to build rpi image
   #  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
   #  nix.settings.extra-platforms = [ "aarch64-linux" ];
