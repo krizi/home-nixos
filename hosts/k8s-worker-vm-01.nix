bbaa3bf3010da1d6f632654a1aeae7d358c04de0
krizi <krazy2krizi@gmail.com>
2025-12-30 02:21:04 +0100

    feat: switch to k0s using existing module

--- hosts/k8s-worker-vm-01.nix
+++ hosts/k8s-worker-vm-01.nix
@@ -3,16 +3,35 @@
 {
   imports = [
     ./hardware/hellga-vm-hardware.nix
-    ../modules/k3s/k3s-common.nix
     ../modules/users/user-kubernetes.nix
-    ../modules/k3s/k3s-agent.nix
     ../modules/nix-management/auto-update.nix
   ];
 
-  networking.hostName = "k8s-worker-vm-01";
-
-  k3s.nodeLabels = {
-    "topology.kubernetes.io/region" = "ch-central";
-    "topology.kubernetes.io/zone" = "hellga";
+  networking = {
+    hostName = "k8s-worker-vm-01";
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [
+        22
+        6443
+      ];
+    };
   };
+
+  environment.etc."k0s/k0stoken".text = builtins.readFile ../tokens/k0s-token.txt;
+
+  # k0s worker service aktivieren
+  services.k0s = {
+    enable = true;
+    role = "worker";
+
+    tokenFile = "/etc/k0s/k0stoken";
+
+    # optional: Labels
+    # extraArgs = [
+    #   "--labels zone=arm"
+    #   "--labels rack=lab"
+    # ];
+  };
+
 }
